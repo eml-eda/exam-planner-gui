f6ed3a8db58de357c925917123d5cac58a1f9924
Moien <137896500+moienheydari@users.noreply.github.com>
2025-11-28 17:49:31 +0100

    github pages fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import { initializeDatabase } from './utils/simpleDatabase';
 import Home from './pages/Home';
