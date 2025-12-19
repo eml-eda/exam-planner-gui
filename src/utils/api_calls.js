064265a63c58108fc98144a92c6bf5da3f4e6e89
Moien <137896500+moienheydari@users.noreply.github.com>
2025-12-19 14:27:11 +0100

    change config api

--- src/utils/api_calls.js
+++ src/utils/api_calls.js
@@ -1,12 +1,29 @@
 import axios from "axios";
 
 
-export const getExamConflicts = async (courseCode) => {
+export const getExamsApi = async (courseCode) => {
     try {
         const response = await axios.get(`http://127.0.0.1:8000/exams_apelli/${courseCode}`);
         return response.data;
     } catch (error) {
         console.error("Error fetching exam data:", error);
+        throw error;
+    }
+};
+
+
+export const reloadDatabaseApi = async (year = null, name = null) => {
+    try {
+        const params = {
+            year: year,
+            name: name
+        };
+
+        const response = await axios.post('http://127.0.0.1:8000/reload_database', null, { params });
+        console.log(response.status);
+        return response.data;
+    } catch (error) {
+        console.error("Error reloading database:", error);
         throw error;
     }
 };
