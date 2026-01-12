4f639cd68964bc09f6347891645c06c44ea5527a
Moien <137896500+moienheydari@users.noreply.github.com>
2026-01-12 10:38:40 +0100

    Added HTTPS url

--- src/utils/api_calls.js
+++ src/utils/api_calls.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-const baseUrl = "http://maira.polito.it:8001";
+// const baseUrl = "http://maira.polito.it:8001";
+const baseUrl = "https://maira.polito.it:8001";
+// const baseUrl = "http://127.0.0.1:8000";
 
 
 export const getExamsApi = async (courseCode) => {
