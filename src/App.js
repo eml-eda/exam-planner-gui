6ce91b3c47a16f50b4f74a04d9d4c466822ec9d5
Moien <137896500+moienheydari@users.noreply.github.com>
2026-03-03 16:30:11 +0100

    Added AuthProvider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
+import { AuthProvider } from './context/AuthContext';
 import { ConfigProvider } from './context/ConfigContext';
 import Home from './pages/Home';
 import Course from './pages/Course';
@@ -9,16 +10,18 @@ import './index.css';
 function App() {
     return (
         <Router>
-            <ConfigProvider>
-                <LanguageProvider>
-                    <div className="App">
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/course/:courseId" element={<Course />} />
-                        </Routes>
-                    </div>
-                </LanguageProvider>
-            </ConfigProvider>
+            <AuthProvider>
+                <ConfigProvider>
+                    <LanguageProvider>
+                        <div className="App">
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/course/:courseId" element={<Course />} />
+                            </Routes>
+                        </div>
+                    </LanguageProvider>
+                </ConfigProvider>
+            </AuthProvider>
         </Router >
     );
 }
